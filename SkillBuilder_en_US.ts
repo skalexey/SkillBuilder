bb1d1cec7ee55a036a65938c4564abd593e67ec9
skalexey <skorohodov-ad@narod.ru>
2022-02-22 18:22:40 +0300

    Update translations

--- SkillBuilder_en_US.ts
+++ SkillBuilder_en_US.ts
@@ -44,11 +44,65 @@
     </message>
 </context>
 <context>
+    <name>DialogRemoveSkillWarning</name>
+    <message>
+        <location filename="UI/DialogRemoveSkillWarning.qml" line="5"/>
+        <location filename="UI/DialogRemoveSkillWarning.qml" line="5"/>
+        <source>Warning!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="UI/DialogRemoveSkillWarning.qml" line="6"/>
+        <location filename="UI/DialogRemoveSkillWarning.qml" line="6"/>
+        <source>This skill has instances in the field. Do you really want to remove it with all it&apos;s instances?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogTemplate</name>
+    <message>
+        <location filename="UI/DialogTemplate.qml" line="6"/>
+        <location filename="UI/DialogTemplate.qml" line="6"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="UI/DialogTemplate.qml" line="17"/>
+        <location filename="UI/DialogTemplate.qml" line="17"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="UI/DialogTemplate.qml" line="18"/>
+        <location filename="UI/DialogTemplate.qml" line="18"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>EditField</name>
     <message>
         <location filename="dependencies/DataModelBuilder/Client/UI/EditField.qml" line="26"/>
         <location filename="dependencies/DataModelBuilder/Client/UI/EditField.qml" line="26"/>
         <source>Enter text</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FileInputRow</name>
+    <message>
+        <location filename="UI/FileInputRow.qml" line="9"/>
+        <location filename="UI/FileInputRow.qml" line="9"/>
+        <source>Row</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LineInputRow</name>
+    <message>
+        <location filename="UI/LineInputRow.qml" line="10"/>
+        <location filename="UI/LineInputRow.qml" line="10"/>
+        <source>Row</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -132,9 +186,45 @@
 <context>
     <name>SkillBuilderUI</name>
     <message>
-        <location filename="UI/SkillBuilderUI.qml" line="35"/>
-        <location filename="UI/SkillBuilderUI.qml" line="35"/>
+        <location filename="UI/SkillBuilderUI.qml" line="42"/>
+        <location filename="UI/SkillBuilderUI.qml" line="42"/>
         <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SkillCreationDialog</name>
+    <message>
+        <location filename="UI/SkillCreationDialog.qml" line="5"/>
+        <location filename="UI/SkillCreationDialog.qml" line="5"/>
+        <source>New skill creation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SkillInfoDialog</name>
+    <message>
+        <location filename="UI/SkillInfoDialog.qml" line="5"/>
+        <location filename="UI/SkillInfoDialog.qml" line="5"/>
+        <source>Skill info</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SkillLibrary</name>
+    <message>
+        <location filename="UI/SkillLibrary.qml" line="27"/>
+        <location filename="UI/SkillLibrary.qml" line="27"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TextInputRow</name>
+    <message>
+        <location filename="UI/TextInputRow.qml" line="11"/>
+        <location filename="UI/TextInputRow.qml" line="11"/>
+        <source>Row</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
