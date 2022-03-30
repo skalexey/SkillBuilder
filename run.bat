a7bf520832a6fb62747d052d64d99ef2b5071a87
skalexey <skorohodov-ad@narod.ru>
2022-03-30 17:08:23 +0300

    Support debug option in run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,17 @@
 @echo off
+
+set destinationFolder=Release
+set argCount=0
+for %%x in (%*) do (
+	set /A argCount+=1
+	if "%%~x" == "debug" (
+		echo --- 'debug' option passed. Use Debug build folder
+		set destinationFolder=Debug
+	)
+)
+
 set dir=%CD%
-set buildDir=%CD%\Build-cmake\Release
+set buildDir=%CD%\Build-cmake\%destinationFolder%
 cd %buildDir%
 call SkillBuilder.exe
 cd %dir%
